6efd8461bdca9dffec8374b50eec9b5c58da46d7
Ibrahim AlDekhail <idekhail@googlemail.com>
2024-06-15 14:11:28 +0300

    Add files via upload

---
+++ Fragment1.kt
@@ -0,0 +1,9 @@
+package com.ibrahim.asb2_createfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class Fragment1 : Fragment(R.layout.fragment1) {
+
+
+}
---
+++ Fragment2.kt
@@ -0,0 +1,11 @@
+package com.ibrahim.asb2_createfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Fragment2 : Fragment(R.layout.fragment2) {
+
+}
